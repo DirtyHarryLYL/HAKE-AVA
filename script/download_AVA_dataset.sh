17ed0ff5eb0d2b0a89236b081d041bb3758a4946
Yong-Lu Li <lylxiaonei@163.com>
2022-01-24 03:23:49 +0800

    Create download_AVA_dataset.sh

---
+++ script/download_AVA_dataset.sh
@@ -0,0 +1,56 @@
+# Download videos
+DATA_DIR="../data/DIO/videos"
+
+if [[ ! -d "${DATA_DIR}" ]]; then
+  echo "${DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${DATA_DIR}
+fi
+
+wget https://s3.amazonaws.com/ava-dataset/annotations/ava_file_names_trainval_v2.1.txt
+
+for line in $(cat ava_file_names_trainval_v2.1.txt)
+do
+  wget https://s3.amazonaws.com/ava-dataset/trainval/$line -P ${DATA_DIR}
+done
+# Cut each video from 15th to 30th minute
+IN_DATA_DIR="../data/DIO/videos"
+OUT_DATA_DIR="../data/DIO/videos_15min"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  out_name="${OUT_DATA_DIR}/${video##*/}"
+  if [ ! -f "${out_name}" ]; then
+    ffmpeg -ss 900 -t 901 -i "${video}" "${out_name}"
+  fi
+done
+# Extract frames
+IN_DATA_DIR="../data/DIO/videos_15min"
+OUT_DATA_DIR="../data/DIO/frames"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  video_name=${video##*/}
+
+  if [[ $video_name = *".webm" ]]; then
+    video_name=${video_name::-5}
+  else
+    video_name=${video_name::-4}
+  fi
+
+  out_video_dir=${OUT_DATA_DIR}/${video_name}/
+  mkdir -p "${out_video_dir}"
+
+  out_name="${out_video_dir}/${video_name}_%06d.jpg"
+
+  ffmpeg -i "${video}" -r 30 -q:v 1 "${out_name}"
+done
